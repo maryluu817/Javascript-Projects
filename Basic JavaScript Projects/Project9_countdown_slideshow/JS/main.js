5ff4ecb7d138d11b1a1bd365afb15a95f29967bd
maryluu817 <109318116+maryluu817@users.noreply.github.com>
2023-04-12 21:52:11 -0400

    Created new folder in JavaScript Projects with assignments.

---
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,16 @@
+function countdown() {
+    var seconds = document.getElementById("seconds").value ;
+    
+    function tick() {
+        seconds = seconds -1
+        timer .innerHTML = seconds;
+        var time =setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's Up!");
+            clearTimeout(time);
+            timer.innerHTML ="";
+        }
+    
+    }
+    tick();
+}
